fba6ed7cd6535db77fd13dc64dfdcf6d3b7604a7
kkangde <rlawogus4135@gmail.com>
2025-06-13 11:25:10 +0900

    ㅇㅇ
    ㄹ

--- 프로젝트/파이썬11.py
+++ 프로젝트/파이썬11.py
@@ -350,4 +350,4 @@ def show_news():
     return render_template('news.html', news=news)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4880)
